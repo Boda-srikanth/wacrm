925f219ea23dbf8887b39f7b89217fa529e71d8c
Boda-srikanth <147930225+Boda-srikanth@users.noreply.github.com>
2026-06-19 10:42:09 +0530

    sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,19 +3,19 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://chatio.tech",
+      url: "https://chatio.satinova.in",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: "https://chatio.tech/login",
+      url: "https://chatio.satinova.in/login",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.3,
     },
     {
-      url: "https://chatio.tech/signup",
+      url: "https://chatio.satinova.in/signup",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
